9d8eac055282f170485f3c40616119d16ab22ef5
Ruderud <jungkh0055@gmail.com>
2021-06-06 12:12:23 +0900

    배열의 항목 삭제구현

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
+import CreateUser from './CreateUser';
 import UserList from './UserList';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email} = inputs
+  const onChange = e => {
+    const { name, value } = e.target; 
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
         id: 1,
         username: '페코린느',
@@ -23,18 +37,35 @@ function App() {
       username: '복제된 캬루',
       email: 'imTraitor@cygames.com',
   }, 
-];
+]); {/* 이때 이 배열에 대해서 splice, sort, push같은함수는 사용하면안된다! 꼭 사용하고싶다면 배열을 복사해서 사용해야함 -> 배열의 불변성을 위함 */}
 
-  const nextId = useRef(4);
+  const nextId = useRef(5);
 
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    //setUsers([...users, user]); //기존배열은 건드리지않으면서 ...users로 이전배열들을 가져오고 그 뒤에 새로 추가된 user 프로퍼티를 추가함
+    setUsers(users.concat(user)); //또는 이방법대신 concat을 사용할수있음.
 
+    setInputs({
+      username: '',
+      email: ''
+    });
     console.log(nextId.current); //4가 나타나게될것.
     nextId.current += 1;  //넥스트아이디값을 마지막에 사용한다음 +1을 시킴 -> 자동으로 다음에 사용할 넥스트아이디값을 갱신시키는 용도
   }
 
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id)); //기존 user리스트를 받아와서 그내부의 id값이입력한 id값과 다르면놔두고, id와 같은거면 지워서 그 프로퍼티들을 배열로 만들어 반환한다.
+  };
   return (
-    <UserList users={users}/>
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+      <UserList users={users} onRemove={onRemove}/>
+    </>
   )
 }
 
--- UserList.js
+++ UserList.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user, onRemove }) {
+    const {username, email, id} = user;
     return (
         <div>
                 <b>{user.username}</b><span>({user.email})</span>
+                <button onClick={() => onRemove(id)}>삭제</button>  
+                {/* 절대 <button onClick={onRemove(id)}>삭제</button> 이런식으로 함수를 호출하지말고, 함수를 만들어서 써야한다! 
+                (호출하는방식이면 삭제를 누르면 랜더링되면서 리스트 전부가 지워져버림)*/}
         </div>
     );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove }) {
     return(
         <div>
         {
             users.map(
-                user => (<User user={user} key={user.id} />)
+                user => (<User user={user} key={user.id} onRemove={onRemove}/>)
             ) 
         }       {/* 각 객체의 키값을 특정할수있는 key(여기서는 id)가 따로 정해져있지 않다면, index값을 가져와서 사용한다. -> 다만 효율적인 상승은 기대할 수 없음
                  (user, index) => (<User user={user} key={index}/>) */}
