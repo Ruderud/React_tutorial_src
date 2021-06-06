f6a689dfb79bb58382b5dffe154f79b686077c65
Ruderud <jungkh0055@gmail.com>
2021-06-06 19:17:13 +0900

    useEffect, useMemo, useCallback함수 사용

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef, useState, useMemo, useCallback } from 'react';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 
+function countActiveUsers(users) {
+  console.log('활성 사용자수를 세는중...');   //useMemo를 사용하기전에는 실제로 user에 등록하지않고 input창에만 값을 타이핑하고있는 그순간마다 랜더링하며 연산을 하게된다.
+  return users.filter(user => user.active).length;
+}
 function App() {
   const [inputs, setInputs] = useState({
     username: '',
     email: '',
   });
   const { username, email} = inputs
-  const onChange = e => {
+
+  const onChange = useCallback( e => {
     const { name, value } = e.target; 
     setInputs({
       ...inputs,
       [name]:value
     });
-  };
+  }, [inputs]);
 
   const [users, setUsers] = useState([
     {
@@ -45,7 +50,7 @@ function App() {
 
   const nextId = useRef(5);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -60,24 +65,27 @@ function App() {
     });
     console.log(nextId.current); //4가 나타나게될것.
     nextId.current += 1;  //넥스트아이디값을 마지막에 사용한다음 +1을 시킴 -> 자동으로 다음에 사용할 넥스트아이디값을 갱신시키는 용도
-  }
+  }, [username, email, users]);
 
-  const onRemove = id => {
+  const onRemove = useCallback( id => {
     setUsers(users.filter(user => user.id !== id)); //기존 user리스트를 받아와서 그내부의 id값이입력한 id값과 다르면놔두고, id와 같은거면 지워서 그 프로퍼티들을 배열로 만들어 반환한다.
-  };
+  }, [users]);
 
-  const onToggle = id => {
+  const onToggle = useCallback( id => {
     setUsers(users.map(
       user => user.id === id 
         ? {...user, active: !user.active} //기존값을 먼저 가져오고, 그중에 바꿔줄값을 함수에따라 덮어씌우는 방식으로 사용한다.
         : user
     ));
-  };
+  }, [users]);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);  //useMemo를 통해서 input에 데이터를 입력할때마다 해당 값을 매번계산하지않고 그냥 값을 불러와서 사용함.
 
   return (
     <>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
       <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성 사용자 수: {count}</div>
     </>
   )
 }
--- UserList.js
+++ UserList.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
     const {username, email, id, active} = user;
+
+    useEffect(() => {
+        console.log('유저값이 설정됨');
+        console.log(user);
+        return () => {                      //뒷정리함수
+            console.log('유저값이 바뀌기전');
+            console.log(user);
+        }
+    }, [user]); //useEffect에서 사용하는 데이터값을 여기 끝(depth배열)에 리스트로 넣어줘야만 최신값을 가르키게할 수 있음. 함수도 마찬가지임. 
+                //값을 할당하지않으면 user단에서 리랜더링되지않고 부모단인 user list 단에서 렌더링이 적용되기때문에, 리스트 내용 전부가 리랜더링된다. (속도저하의 요인) -> 컴포넌트 리랜더링 최적화
+
+    // useEffect(() => {
+    //     console.log('컴포넌트가 화면에 나타남'); // 데이터를 등록하면 해당 문구가 콘솔창에 나타남
+    //     // props -> stare, REST API, D3 Video.js, setInterval, setTimeout등의 작업가능
+    //     return () => {
+    //         console.log('컴포넌트가 화면에서 사라짐'); // 데이터를 삭제하면 해당 문구가 콘솔창에 나타남
+    //         // setInterval, setTimeout의 작업을 끝낼때 -> 즉 clearInterval/Timeout을 사용할때, 라이브러리 인스턴스 제거
+    //     }
+    // }, []);
+
     return (
         <div>
                 <b style={{
